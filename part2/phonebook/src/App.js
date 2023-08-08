85e5cb611a9054832751af305082fb1da007e841
Kevin Dormer <krdormer_dev@protonmail.com>
2023-08-07 23:01:12 -0500

    [FSO] Complete Ex. 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,20 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newContact, setNewContact] = useState({ name: "", number: "" });
+
+  const handleFilter = (event) => {
+    console.log(event.target.value);
+    const filteredPersons = persons.filter((person) =>
+      person.name.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    return setPersons(filteredPersons);
+  };
 
   const handleContactChange = (event) => {
     setNewContact({
@@ -33,6 +44,15 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{" "}
+        <input
+          name="filter"
+          onChange={handleFilter}
+          value={newContact.filter}
+        />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleAddContact}>
         <div>
           name:{" "}
