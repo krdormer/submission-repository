02883692331dc61f5b2514f3ebfaef02f0c30f1e
Kevin Dormer <krdormer_dev@protonmail.com>
2023-08-07 22:29:58 -0500

    [FSO] Complete Ex. 2.4

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -3,13 +3,18 @@ import Header from "./Header";
 import Content from "./Content";
 import Total from "./Total";
 
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
+  console.log("🚀 ~ file: Course.js:7 ~ Course ~ courses:", courses);
   return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
+    <>
+      {courses.map((course) => (
+        <div>
+          <Header course={course.name} />
+          <Content parts={course.parts} />
+          <Total parts={course.parts} />
+        </div>
+      ))}
+    </>
   );
 };
 
