3e487edafa4db4b277971e991af015192f6e92cd
Kevin Dormer <krdormer_dev@protonmail.com>
2023-08-07 20:54:58 -0500

    [FSO] Complete Ex. 1.9

--- part1/unicafe/src/components/Stats.js
+++ part1/unicafe/src/components/Stats.js
@@ -1,18 +1,23 @@
 const Stats = ({ good, neutral, bad, total }) => {
   const averageRating = (good - bad) / total;
   const positiveRating = (good / total) * 100;
+  const feedbackGiven = (good || neutral || bad) > 0;
 
   return (
     <>
       <h2>Statistics</h2>
-      <div>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
-        <p>All: {total}</p>
-        <p>Average: {averageRating}</p>
-        <p>Positive: {positiveRating}%</p>
-      </div>
+      {feedbackGiven ? (
+        <div>
+          <p>Good: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+          <p>All: {total}</p>
+          <p>Average: {averageRating}</p>
+          <p>Positive: {positiveRating}%</p>
+        </div>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </>
   );
 };
